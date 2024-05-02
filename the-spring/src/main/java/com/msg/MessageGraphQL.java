fd335d05e4fd5adba0d9f7078fa20c34907face7
PercyJiang <jiangpercy@outlook.com>
2024-05-02 15:57:11 -0400

    graphql

--- the-spring/src/main/java/com/msg/MessageGraphQL.java
+++ the-spring/src/main/java/com/msg/MessageGraphQL.java
@@ -1,6 +1,8 @@
 package com.msg;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -19,5 +21,10 @@ public class MessageGraphQL {
   @QueryMapping
   public List<Message> graphGetAll() {
     return messageService.getAll();
+  }
+
+  @MutationMapping
+  public Integer graphPost(@Argument("source") String source, @Argument("content") String content) {
+    return messageService.create(source, content);
   }
 }
