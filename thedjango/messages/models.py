92d3ac02b401299edf90578cb4696efba3210d5e
PercyJiang <jiangpercy@outlook.com>
2025-03-04 10:13:49 -0500

    messages

---
+++ thedjango/messages/models.py
@@ -0,0 +1,21 @@
+import uuid
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class Client(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    created = models.DateTimeField(default=timezone.localtime)
